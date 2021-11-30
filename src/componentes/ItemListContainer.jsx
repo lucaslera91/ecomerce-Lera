92121b9c987e86a813b25a93941639bcd84852ec
lucaslera91 <lucaslera91@gmail.com>
2021-11-29 23:52:25 -0300

    setContador

--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Children } from 'react'
+import react,{ useState } from "react";
 
-export default function ItemListContainer({greeting}) {
+export default function ItemListContainer({greeting, Children}) {
     
     let containerHeight = "100vh"
-   
+   const [contador, setContador] = useState(0);
     return (
         
-        <div className="border border-dark bg-light d-flex align-items-center justify-content-center" style={{height:  containerHeight}}>
+        <div className="border border-dark bg-light d-flx align-content-center justify-content-center" style={{height:  containerHeight}}>
             
             <h2>{greeting}</h2>
+
+            <h2>{contador}</h2>
+
+
+            <button onClick={()=>setContador(contador+1)}>+</button>
+
+            <button onClick={()=> setContador(contador-1)}>-</button>
            
         </div>
     )
