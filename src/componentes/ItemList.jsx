771850f88dc72c513ace91170c0fef53523179e3
lucaslera91 <lucaslera5@gmail.com>
2021-12-02 10:43:42 -0300

    title

--- src/componentes/ItemList.jsx
+++ src/componentes/ItemList.jsx
@@ -8,15 +8,9 @@ function ItemList({items}) {
 
         if(Array.isArray(items)){
             return items.map((item) => <Item key={item.id} item={item}></Item>);
-            //console.log(typeof listaItem)
         }else{
             return <h3>Loading...</h3>
-            //alert('not array')
         }
-    // return (
-    //      <div>
-    //         {}
-    //      </div>
-    //  )
+   
 }
 export default ItemList;
