7ffdb016c4b658e2d13a44f0b9cba558b10ed10b
lucaslera91 <lucaslera5@gmail.com>
2021-12-21 11:02:52 -0300

    cart context

--- src/componentes/ButtonCounter.jsx
+++ src/componentes/ButtonCounter.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import react, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import BtnAddToCart from './BtnAddToCart';
+import { CartConsumer } from '../context/CartProvider'
 
 
-function ButtonCounter({start, stock, text}) {
+
+function ButtonCounter({start, stock, text, item}) {
     const [contador, setContador] = useState(start);
-    const [display, setDisplay] = useState(`visible`)
-    const [resultado, setDisplayResultado] = useState(`invisible`)
-    const [compra, setCompra] = useState(0)
-    const classAux = `${display} container-fluid d-flex justify-content-between w-auto flex-column`
-    const classTextAux = `bg-transparent text-${text} border-0 w-100`
-    const resultadoClass = `${resultado} bg-primary text-dark rounded`
     
+    const [display, setDisplay] = useState(`visible`)
+    const [resultado, setDisplayResultado] = useState(`d-none`)
+    const [compra, setCompra] = useState(0)
+    const [addMore, setAddMore] = useState('d-none')
+
+    const classAddMore = `${addMore} bg-light w-100 border border-primary rounded text-primary my-1`
+    const classAux = `${display} container-fluid d-flex justify-content-between flex-column w-100 p-0 my-1`
+    const classTextAux = `bg-transparent text-${text} border-0 w-100`
+    const resultadoClass = `${resultado} bg-primary text-dark rounded my-1`
+    const {cart, addCart} = CartConsumer();
     const onAdd = () => {
         if (contador < stock) {
             setContador(contador + 1)
@@ -23,27 +29,37 @@ function ButtonCounter({start, stock, text}) {
             setContador(contador - 1)
         }
     }
-    function handleAddToCart(){
-         setDisplay('invisible')
+    function handleAddMore(){
+        setDisplay('visible')
+         setDisplayResultado('d-none')
+         setAddMore('d-none')
+    }
+    function handleAddToCart(item){
+         setDisplay('d-none')
          setDisplayResultado('visible')
          setCompra(contador)
+         addCart(item, contador)
+         //alert(contador)
+         setAddMore('visible')
     }
+
 
    
     return (
-        <div>
+        <div className='p-1' style={{minHeight: '110px'}}>
             <div  className={classAux}>
-                <div className='d-flex'>
-                <button className={classTextAux} onClick={onAdd}><i className="fas fa-plus"></i></button>
+                <div className='d-flex w-100'>
+                    <button className={classTextAux} onClick={onAdd}><i className="fas fa-plus"></i></button>
                     <h3>{contador}</h3>
                     <button className={classTextAux} onClick={less}><i className="fas fa-minus"></i></button>
                 </div>
-                <div>
-                    <button onClick={handleAddToCart} className="bg-light w-100 border border-primary rounded text-primary p-0">Add to Cart</button>
+                <div className='w-100 '>
+                    <button onClick={() => handleAddToCart(item)} className="bg-light w-100 border border-primary rounded text-primary p-0">Add to Cart</button>
                 </div>
-                </div>
-                <div className={resultadoClass}>Product added to cart!</div>
-                <Link className='text-decoration-none' to="/cart"><div className='m-2 bg-light w-auto border border-primary rounded text-primary p-0" '>Go to cart!</div></Link>
+            </div>
+            <div><button onClick={handleAddMore} className={classAddMore}>Add more to cart</button></div>
+            <div className={resultadoClass}>Product added to cart!</div>
+            <Link className='text-decoration-none w-100 ' to="/cart"><div className='bg-light w-auto border border-primary rounded text-primary p-0" '>Go to cart!</div></Link>
         </div>);
       
 }
