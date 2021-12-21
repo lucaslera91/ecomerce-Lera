1e8ed29dfdee44296b9d60ed12dfbac6ba87d70a
lucaslera91 <lucaslera5@gmail.com>
2021-12-21 12:02:50 -0300

    car widget

--- src/componentes/CartWidget.jsx
+++ src/componentes/CartWidget.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function CartWidget({color}) {
-    let classColor = `fas fa-shopping-cart text-${color}`
+    let classColor = `fas fa-shopping-cart text-${color} px-2`
     return (
-        <div>
+        <div classNAm='text-decoration-none'>
           <i className={classColor}></i>
         </div>
     )
