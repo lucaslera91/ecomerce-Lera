69fcec906ff9b27af11ca44f7455e398eabd54f1
lucaslera91 <lucaslera91@gmail.com>
2022-01-20 19:14:02 -0300

    design

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ import ItemDetailContainer from './componentes/ItemDetailContainer';
 import Ruta from './Rutas/Ruta';
 function App() {
  
+  //const url ='https://previews.123rf.com/images/sabelskaya/sabelskaya1906/sabelskaya190601293/128171042-terrazzo-style-hand-drawn-seamless-pattern-background-with-big-abstract-speckles-for-marble-stone-or.jpg'
+  const url ='https://wallpaperaccess.com/full/1216450.jpg'
+  //const url =''
+  //const url =''
+  //const url =''
+  //const url =''
+  //const url =''
+  //const url =''
+  //const url =''
+  //const url =''
+  //const url =''
+  //const url =''
 
   return (
-    <div className="App" style={{backgroundImage: `url(https://wallpaperaccess.com/full/1288360.jpg)`}}>
+  
+    <div className="App" style={{backgroundImage: `url(${url})`}}>
       <Ruta/>
     </div>
 
