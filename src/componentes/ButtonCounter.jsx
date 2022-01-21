a739c5c5ffc6d9905e50446cf50a3cab1510f8f1
lucaslera91 <lucaslera91@gmail.com>
2022-01-21 15:15:21 -0300

    kj

--- src/componentes/ButtonCounter.jsx
+++ src/componentes/ButtonCounter.jsx
@@ -14,7 +14,7 @@ function ButtonCounter({start, stock, item}) {
 
     const classAddMore = `${addMore} bg-light w-100 border border-primary rounded text-primary my-1`
     const classAux = `${display} container-fluid d-flex justify-content-between flex-column w-100 p-0 my-1`
-    const classTextAux = `bg-transparent text-${'daek'} border-0 w-100`
+    const classTextAux = `bg-transparent text-${'dark'} border-0 w-100`
     const resultadoClass = `${resultado} bg-primary text-dark rounded my-1`
     const {addCart} = CartConsumer();
     const onAdd = () => {
