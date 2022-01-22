38e2f0b0a864c27296dcd6eacf3d9fbd8974ba3c
lucaslera91 <lucaslera91@gmail.com>
2022-01-22 17:10:24 -0300

    main page

--- src/componentes/Item.jsx
+++ src/componentes/Item.jsx
@@ -8,7 +8,7 @@ function Item({item, fn, text}) {
    
 
     return (
-        <div className='col-4 m-2 text-dark' style={{width: 200}}>
+        <div className='col-4 m-2 text-dark' style={{minWidth: '300px', maxWidth: "400px"}}>
             <div className="container-fluid w-100 border-bottom border-dark p-1 bg-transparent">
             <div className='w-100 py-1'>
                 <h3 className='d-flex justify-content-center align-items-center text-dark' style={{height: 60}}>{item.title}</h3>
@@ -18,7 +18,7 @@ function Item({item, fn, text}) {
             </div>
             <div>
                 <Link to={`/detail/${item.id}`}>
-                <button className="bg-light w-100 border border-dark my-1 rounded text-dark" >Ver mas detalles!</button>
+                <button className="bg-light w-50 border border-dark my-1 rounded text-dark" >Ver mas detalles!</button>
                 </Link>
             </div>
             <div>
