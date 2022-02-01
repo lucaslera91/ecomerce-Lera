74545dc54fd310780973de33f9a0460afd6da288
lucaslera91 <lucaslera5@gmail.com>
2022-02-01 14:36:06 -0300

    save

--- src/componentes/CartView.jsx
+++ src/componentes/CartView.jsx
@@ -11,7 +11,7 @@ function CartView() {
     if (cart.length < 1){
         return <div className='text-dark' style={{minHeight: '100vh'}}>
                 <h3>No items in cart. Make your first purchase</h3>
-                <Link to='/home'><button className='bg-light rounded text-primary my-1'>Back to home</button></Link>
+                <Link to='/'><button className='bg-light rounded text-primary my-1'>Back to home</button></Link>
             </div>
     }else{
         return (
