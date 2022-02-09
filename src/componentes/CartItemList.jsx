3d835914c90e5dcbaae079330686c2bdf7919261
lucaslera91 <lucaslera5@gmail.com>
2022-02-09 20:51:42 -0300

    final touches

--- src/componentes/CartItemList.jsx
+++ src/componentes/CartItemList.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import CartItem from './CartItem'
-import { CartConsumer } from '../context/CartProvider'
 
 function CartItemList({items}) {
-    const {removeElement} = CartConsumer();
     
     if(items.length > 0){
     return (
@@ -14,14 +12,13 @@ function CartItemList({items}) {
                   <th scope="col">Item</th>
                   <th scope="col">Precio</th>
                   <th scope="col">Cantidad</th>
-                 
                   <th scope="col">Eliminar</th>
                 </tr>
             </thead>
             <tbody>
             {items.map((element) => 
                         <CartItem item={element} count={items.indexOf(element)} id={element.id} key={element.id}></CartItem>
-                 )}
+            )}
             </tbody>
         </table>
            )
@@ -30,7 +27,7 @@ function CartItemList({items}) {
         return (
                 <div>
                     <h3>Add your First Item!</h3>
-                    <a className='text-decoration-none text-dark' href="../"><button className='bg-light w-25 border border-primary rounded text-primary my-1'>Go to Home!</button></a>
+                    <a className='text-decoration-none text-dark' href="../"><button className='bg-light border-primary w-25 border rounded text-primary my-1'>Go to Home!</button></a>
                 </div>)
 }
 }
